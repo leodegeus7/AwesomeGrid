c369062db8529d9897c219171d1ceac2391c79af
Leonardo Geus <leoardodegeus@gmail.com>
2018-01-04 14:36:23 -0200

    Updated Demo App

--- DemoApp/ViewController.swift
+++ DemoApp/ViewController.swift
@@ -22,54 +22,34 @@ class ViewController: UIViewController,GridLayoutDelegate {
         collectionView.initialize(numberOfColumns: 4, debug: true ,optionalPadding: 16)
         self.view.backgroundColor = UIColor.darkGray
         collectionView.longPressToMove = true
-        collectionView.backgroundColor = UIColor.darkGray
         collectionView.tapToResize = false
     }
     
-    var count = 0
     
     func getCellsSupport() -> [CellSupport] {
-        let colors = Colors().getColors()
         var cells = [CellSupport]()
-        
         let cell1 = CellSupport(gridView: collectionView, row: 0, column: 0, squaresOfHeight: 1, squaresOfWidth: 4)
         cells.append(cell1)
-        let cell3 = CellSupport(gridView: collectionView, row: 1, column: 0, squaresOfHeight: 2, squaresOfWidth: 2)
-        cells.append(cell3)
-        let cell4 = CellSupport(gridView: collectionView, row: 1, column: 2, squaresOfHeight: 1, squaresOfWidth: 1)
-        cells.append(cell4)
-        let cell5 = CellSupport(gridView: collectionView, row: 1, column: 3, squaresOfHeight: 1, squaresOfWidth: 1)
-        cells.append(cell5)
-        let cell6 = CellSupport(gridView: collectionView, row: 2, column: 2, squaresOfHeight: 1, squaresOfWidth: 2)
-        cells.append(cell6)
-        let cell7 = CellSupport(gridView: collectionView, row: 3, column: 0, squaresOfHeight: 1, squaresOfWidth: 1)
-        cells.append(cell7)
-        let cell8 = CellSupport(gridView: collectionView, row: 3, column: 1, squaresOfHeight: 1, squaresOfWidth: 1)
-        cells.append(cell8)
-        let cell9 = CellSupport(gridView: collectionView, row: 3, column: 2, squaresOfHeight: 1, squaresOfWidth: 1)
-        cells.append(cell9)
-        let cell10 = CellSupport(gridView: collectionView, row: 3, column: 3, squaresOfHeight: 1, squaresOfWidth: 1)
-        cells.append(cell10)
-        let cell11 = CellSupport(gridView: collectionView, row: 4, column: 0, squaresOfHeight: 2, squaresOfWidth: 4)
-        cells.append(cell11)
-        let cell12 = CellSupport(gridView: collectionView, row: 6, column: 0, squaresOfHeight: 1, squaresOfWidth: 1)
-        cells.append(cell12)
-        let cell13 = CellSupport(gridView: collectionView, row: 6, column: 1, squaresOfHeight: 1, squaresOfWidth: 1)
-        cells.append(cell13)
-        let cell14 = CellSupport(gridView: collectionView, row: 6, column: 2, squaresOfHeight: 1, squaresOfWidth: 1)
-        cells.append(cell14)
-        let cell15 = CellSupport(gridView: collectionView, row: 6, column: 3, squaresOfHeight: 1, squaresOfWidth: 1)
-        cells.append(cell15)
-
-        var count = 0
-        for cell in cells {
-            cell.view.backgroundColor = colors[count]
-            count = count + 1
-            if count >= colors.count {
-                count = 0
-            }
-        }
+        cell1.view.backgroundColor = UIColor.red
         
+        
+        let cell2 = CellSupport(gridView: collectionView, row: 1, column: 0, squaresOfHeight: 2, squaresOfWidth: 2)
+        cells.append(cell2)
+        cell2.view.backgroundColor = UIColor.blue
+        
+
+        let cell3 = CellSupport(gridView: collectionView, row: 1, column: 2, squaresOfHeight: 2, squaresOfWidth: 2)
+        cells.append(cell3)
+        cell3.view.backgroundColor = UIColor.green
+        
+        let cell4 = CellSupport(gridView: collectionView, row: 3, column: 0, squaresOfHeight: 2, squaresOfWidth: 2)
+        cells.append(cell4)
+        cell4.view.backgroundColor = UIColor.green
+        
+        
+        let cell5 = CellSupport(gridView: collectionView, row: 3, column: 2, squaresOfHeight: 2, squaresOfWidth: 2)
+        cells.append(cell5)
+        cell5.view.backgroundColor = UIColor.blue
         return cells
     }
     
